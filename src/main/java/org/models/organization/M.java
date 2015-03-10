eb4a46074f1d630977c49369aeeeeee10fccd2b1
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-11 01:06:53 +0800

    suppress javadoc warnings

--- src/main/java/org/models/organization/M.java
+++ src/main/java/org/models/organization/M.java
@@ -1,7 +1,6 @@
 package org.models.organization;
 
+@SuppressWarnings("javadoc")
 public interface M {
-
 	public static final String EXCEPTION_PARAMETER_CANNOT_BE_NULL = "Parameter (%s) cannot be null";
-
 }
