13d658d496da73ad0fce8e4010b36b7967e73317
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-11 00:25:13 +0800

    moved the default role goodness function into its own class

--- src/api/java/org/models/organization/function/RoleGoodnessFunction.java
+++ src/api/java/org/models/organization/function/RoleGoodnessFunction.java
@@ -10,8 +10,6 @@ package org.models.organization.function;
 import java.util.Collection;
 
 import org.models.organization.entity.Agent;
-import org.models.organization.entity.Attribute;
-import org.models.organization.entity.Capability;
 import org.models.organization.entity.InstanceGoal;
 import org.models.organization.entity.Role;
 import org.models.organization.relation.Assignment;
@@ -24,52 +22,6 @@ import org.models.organization.relation.Assignment;
  * @since 6.0
  */
 public interface RoleGoodnessFunction {
-
-	/**
-	 * The <code>DefaultRoleGoodnessFunction</code> provides a very basic goodness function that only checks if the given <code>Agent</code> has the necessary
-	 * attributes and then multiplies the scores of the capabilities.
-	 *
-	 * @author Christopher Zhong
-	 * @since 6.0
-	 */
-	public static final class DefaultRoleGoodnessFunction implements RoleGoodnessFunction {
-
-		@Override
-		public double goodness(final Role role, final Agent agent, final InstanceGoal<?> goal, final Collection<Assignment> assignments) {
-			/*
-			 * first, check that the agent has all the necessary attributes. otherwise, return a null to indicate that the agent cannot play the role
-			 */
-			for (final Attribute attribute : role.getNeedsSet()) {
-				if (agent.has(attribute) == null) {
-					return MIN_SCORE;
-				}
-			}
-			double score = MAX_SCORE;
-			int count = 0;
-			for (final Capability capability : role.getRequiresSet()) {
-				final double possessesScore = agent.possesses(capability);
-				score *= possessesScore;
-				if (Double.compare(score, 0.0) == 0) {
-					/* short circuit */
-					return MIN_SCORE;
-				}
-				count++;
-			}
-
-			if (count < 2) {
-				return score;
-			} else {
-				final double inverse = 1d / count;
-				return Math.pow(score, inverse);
-			}
-		}
-
-		@Override
-		public String toString() {
-			return "Default Role Goodness Function";
-		}
-
-	}
 
 	/**
 	 * <code>MIN_SCORE</code> is the minimum possible value for a score, which is {@value} .
