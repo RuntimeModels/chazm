88e07407495dae8a0456ea951514be0c35c0bec7
Christopher Zhong <ChrisZhong@GMail.com>
2015-03-18 12:50:04 +0800

    fixed compilation errors in XmlParser, does not mean it works

--- src/main/java/model/organization/parsers/XmlParser.java
+++ src/main/java/model/organization/parsers/XmlParser.java
@@ -1,11 +1,6 @@
-/*
- * XMLParser.java
- *
- * Created on Dec 18, 2007
- *
- * See License.txt file the license agreement.
- */
 package model.organization.parsers;
+
+import static model.organization.validation.Checks.checkNotNull;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,19 +11,22 @@ import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 
 import model.organization.Organization;
+import model.organization.entity.Capability;
+import model.organization.entity.EntityFactory;
+import model.organization.entity.Role;
 import model.organization.entity.SpecificationGoal;
+import model.organization.id.IdFactory;
 import model.organization.id.UniqueId;
-import model.organization.id.UniqueIdFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 
-import edu.ksu.cis.agenttool.core.model.Capability;
 import edu.ksu.cis.agenttool.core.model.Goal;
 import edu.ksu.cis.agenttool.core.model.ModelElement;
 import edu.ksu.cis.agenttool.core.model.ModelElement.ModelElementType;
 import edu.ksu.cis.agenttool.core.model.Relationship;
 import edu.ksu.cis.agenttool.core.model.Relationship.RelationshipType;
-import edu.ksu.cis.agenttool.core.model.Role;
 import edu.ksu.cis.agenttool.core.model.Schema;
 import edu.ksu.cis.agenttool.core.model.Schema.DiagramType;
 import edu.ksu.cis.agenttool.core.xml.XMLParseException;
@@ -41,10 +39,7 @@ import edu.ksu.cis.agenttool.core.xml.XMLParseException;
  */
 public class XmlParser {
 
-	/**
-	 * Used only for debugging purposes.
-	 */
-	private static final boolean DEBUG = false;
+	private static final Logger logger = LoggerFactory.getLogger(XmlParser.class);
 
 	/**
 	 * Instantiates an <code>Organization</code> based on the given <code>Path</code>.
@@ -59,9 +54,9 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path) throws SAXException, IOException, ParserConfigurationException {
-		return parse(path, new DefaultGoalFactory(), new DefaultIdFactory(), new OrganizationImpl());
-	}
+	// public static Organization parse(final Path path) throws SAXException, IOException, ParserConfigurationException {
+	// return parse(path, new DefaultGoalFactory(), new DefaultIdFactory(), new OrganizationImpl());
+	// }
 
 	/**
 	 * Instantiates an <code>Organization</code> based on the given <code>Path</code> and <code>SpecificationGoalProvider</code>.
@@ -78,10 +73,10 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final GoalFactory specificationGoalProvider) throws SAXException, IOException,
-			ParserConfigurationException {
-		return parse(path, specificationGoalProvider, new DefaultIdFactory(), new OrganizationImpl());
-	}
+	// public static Organization parse(final Path path, final GoalFactory specificationGoalProvider) throws SAXException, IOException,
+	// ParserConfigurationException {
+	// return parse(path, specificationGoalProvider, new DefaultIdFactory(), new OrganizationImpl());
+	// }
 
 	/**
 	 * Instantiates an <code>Organization</code> based on the given <code>Path</code> and <code>UniqueIdentifierProvider</code>.
@@ -98,9 +93,9 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final UniqueIdFactory uniqueIdFactory) throws SAXException, IOException, ParserConfigurationException {
-		return parse(path, new DefaultGoalFactory(), uniqueIdFactory, new OrganizationImpl());
-	}
+	// public static Organization parse(final Path path, final UniqueIdFactory uniqueIdFactory) throws SAXException, IOException, ParserConfigurationException {
+	// return parse(path, new DefaultGoalFactory(), uniqueIdFactory, new OrganizationImpl());
+	// }
 
 	/**
 	 * Populates the given <code>Organization</code> based on the given <code>Path</code>.
@@ -118,9 +113,9 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final Organization organization) throws SAXException, IOException, ParserConfigurationException {
-		return parse(path, new DefaultGoalFactory(), new DefaultIdFactory(), organization);
-	}
+	// public static Organization parse(final Path path, final Organization organization) throws SAXException, IOException, ParserConfigurationException {
+	// return parse(path, new DefaultGoalFactory(), new DefaultIdFactory(), organization);
+	// }
 
 	/**
 	 * Instantiates an <code>Organization</code> based on the given <code>Path</code>, <code>SpecificationGoalProvider</code>, and
@@ -141,10 +136,11 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final GoalFactory specificationGoalProvider, final UniqueIdFactory uniqueIdFactory) throws SAXException,
-			IOException, ParserConfigurationException {
-		return parse(path, specificationGoalProvider, uniqueIdFactory, new OrganizationImpl());
-	}
+	// public static Organization parse(final Path path, final GoalFactory specificationGoalProvider, final UniqueIdFactory uniqueIdFactory) throws
+	// SAXException,
+	// IOException, ParserConfigurationException {
+	// return parse(path, specificationGoalProvider, uniqueIdFactory, new OrganizationImpl());
+	// }
 
 	/**
 	 * Populates the given <code>Organization</code> based on the given <code>Path</code> and <code>SpecificationGoalProvider</code>.
@@ -163,10 +159,10 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final GoalFactory specificationGoalProvider, final Organization organization) throws SAXException,
-			IOException, ParserConfigurationException {
-		return parse(path, specificationGoalProvider, new DefaultIdFactory(), organization);
-	}
+	// public static Organization parse(final Path path, final GoalFactory specificationGoalProvider, final Organization organization) throws SAXException,
+	// IOException, ParserConfigurationException {
+	// return parse(path, specificationGoalProvider, new DefaultIdFactory(), organization);
+	// }
 
 	/**
 	 * Populates the given <code>Organization</code> based on the given <code>Path</code> and <code>UniqueIdentifierProvider</code>.
@@ -185,10 +181,11 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final UniqueIdFactory uniqueIdFactory, final Organization organization) throws SAXException, IOException,
-			ParserConfigurationException {
-		return parse(path, new DefaultGoalFactory(), uniqueIdFactory, organization);
-	}
+	// public static Organization parse(final Path path, final UniqueIdFactory uniqueIdFactory, final Organization organization) throws SAXException,
+	// IOException,
+	// ParserConfigurationException {
+	// return parse(path, new DefaultGoalFactory(), uniqueIdFactory, organization);
+	// }
 
 	/**
 	 * Populates the given <code>Organization</code> based on the given <code>Path</code>, <code>SpecificationGoalProvider</code>, and
@@ -196,7 +193,7 @@ public class XmlParser {
 	 *
 	 * @param path
 	 *            the <code>Path</code> which contains the necessary information for construction an <code>Organization</code>.
-	 * @param specificationGoalProvider
+	 * @param entityFactory
 	 *            the <code>SpecificationGoalProvider</code> in which to retrieve the <code>SpecificationGoal</code>.
 	 * @param idFactory
 	 *            the <code>UniqueIdentifierProvider</code> that produces <code>UniqueIdentifier</code>.
@@ -212,24 +209,22 @@ public class XmlParser {
 	 * @throws SAXException
 	 *             if any parse errors occur.
 	 */
-	public static Organization parse(final Path path, final GoalFactory specificationGoalProvider, final UniqueIdFactory idFactory,
-			final Organization organization) throws SAXException, IOException, ParserConfigurationException {
-		if (path == null || specificationGoalProvider == null || idFactory == null || organization == null) {
-			throw new IllegalArgumentException(String.format(
-					"Parameters (file: %s, Specification goal provider: %s, unique identifier provider: %s, organization: %s) cannot be null ", path,
-					specificationGoalProvider, idFactory, organization));
-		}
+	public static Organization parse(final Path path, final EntityFactory entityFactory, final IdFactory idFactory, final Organization organization)
+			throws SAXException, IOException, ParserConfigurationException {
+		checkNotNull(path, "path");
+		checkNotNull(entityFactory, "entityFactory");
+		checkNotNull(idFactory, "idFactory");
+		checkNotNull(organization, "organization");
 		if (Files.exists(path)) {
-			if (DEBUG) {
-				System.out.println(String.format("Loading file (%s)", path));
-			}
+			logger.debug("Loading file ({})", path);
 			final edu.ksu.cis.agenttool.core.xml.XMLParser parser = new edu.ksu.cis.agenttool.core.xml.XMLParser(path.toString());
 			try {
 				final Schema schema = parser.getSchema(DiagramType.ROLE);
 				/* parse all the goals */
 				for (final ModelElement element : schema.getChildren(ModelElementType.GOAL)) {
 					final Goal goal = (Goal) element;
-					final SpecificationGoal specificationGoal = specificationGoalProvider.buildGoal(idFactory, goal.getName());
+					final SpecificationGoal specificationGoal = entityFactory
+							.buildSpecificationGoal(idFactory.buildId(SpecificationGoal.class, goal.getName()));
 					if (specificationGoal == null) {
 						throw new IllegalArgumentException(String.format("Cannot find the '%s' specification goal in the given provider", goal.getName()));
 					}
@@ -237,19 +232,18 @@ public class XmlParser {
 				}
 				/* parse all the capabilities */
 				for (final ModelElement element : schema.getChildren(ModelElementType.CAPABILITY)) {
-					final Capability capability = (Capability) element;
-					final UniqueId<model.organization.entity.Capability> capabilityId = idFactory.buildId(model.organization.entity.Capability.class,
-							capability.getName());
-					final model.organization.entity.Capability c = new CapabilityEntity(capabilityId);
+					final edu.ksu.cis.agenttool.core.model.Capability capability = (edu.ksu.cis.agenttool.core.model.Capability) element;
+					final UniqueId<Capability> id = idFactory.buildId(Capability.class, capability.getName());
+					final Capability c = entityFactory.buildCapability(id);
 					organization.addCapability(c);
 				}
 				/* parse all the roles */
 				for (final ModelElement element : schema.getChildren(ModelElementType.ROLE)) {
-					final Role role = (Role) element;
+					final edu.ksu.cis.agenttool.core.model.Role role = (edu.ksu.cis.agenttool.core.model.Role) element;
 					/* only roles that are not inherited are added */
 					if (role.getDestRelationships(RelationshipType.INHERITS).size() == 0) {
-						final UniqueId<model.organization.entity.Role> roleId = idFactory.buildId(model.organization.entity.Role.class, role.getName());
-						final model.organization.entity.Role r = new RoleEntity(roleId);
+						final UniqueId<Role> roleId = idFactory.buildId(Role.class, role.getName());
+						final Role r = entityFactory.buildRole(roleId);
 						organization.addRole(r);
 						/* set up the achieves relation */
 						for (final Relationship achieves : role.getSrcRelationships(RelationshipType.ACHIEVES)) {
@@ -259,18 +253,17 @@ public class XmlParser {
 						}
 						/* set up the requires relation */
 						for (final Relationship requires : role.getSrcRelationships(RelationshipType.REQUIRES)) {
-							final Capability capability = (Capability) requires.getChild();
-							final UniqueId<model.organization.entity.Capability> capabilityId = idFactory.buildId(model.organization.entity.Capability.class,
-									capability.getName());
+							final edu.ksu.cis.agenttool.core.model.Capability capability = (edu.ksu.cis.agenttool.core.model.Capability) requires.getChild();
+							final UniqueId<Capability> capabilityId = idFactory.buildId(Capability.class, capability.getName());
 							organization.addRequires(roleId, capabilityId);
 						}
 						/* set up requires relation from inheritance */
 						for (final List<Relationship> inherits = role.getSrcRelationships(RelationshipType.INHERITS); !inherits.isEmpty();) {
-							final Role parent = (Role) inherits.remove(0).getChild();
+							final edu.ksu.cis.agenttool.core.model.Role parent = (edu.ksu.cis.agenttool.core.model.Role) inherits.remove(0).getChild();
 							for (final Relationship requires : parent.getSrcRelationships(RelationshipType.REQUIRES)) {
-								final Capability capability = (Capability) requires.getChild();
-								final UniqueId<model.organization.entity.Capability> capabilityId = idFactory.buildId(
-										model.organization.entity.Capability.class, capability.getName());
+								final edu.ksu.cis.agenttool.core.model.Capability capability = (edu.ksu.cis.agenttool.core.model.Capability) requires
+										.getChild();
+								final UniqueId<Capability> capabilityId = idFactory.buildId(Capability.class, capability.getName());
 								organization.addRequires(roleId, capabilityId);
 							}
 							inherits.addAll(parent.getSrcRelationships(RelationshipType.INHERITS));
@@ -284,6 +277,7 @@ public class XmlParser {
 				throw new ParserConfigurationException(e.getMessage());
 			}
 		} else {
+			logger.info("File ({}) does not exists", path.toString());
 			throw new FileNotFoundException(String.format("File (%s) does not exists!", path.toString()));
 		}
 		return organization;
