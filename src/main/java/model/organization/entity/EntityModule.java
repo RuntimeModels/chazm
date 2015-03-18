d75512c5c2f5b5d661ab1f791d10b73c0a7d64fd
Christopher Zhong <ChrisZhong@GMail.com>
2015-03-18 12:31:36 +0800

    extracted interfaces from EntityFactory so that each entity has their own factory (i.e. AgentFactory, etc)

--- src/main/java/model/organization/entity/EntityModule.java
+++ src/main/java/model/organization/entity/EntityModule.java
@@ -1,5 +1,7 @@
 package model.organization.entity;
 
+import model.organization.id.IdModule;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
@@ -13,10 +15,18 @@ public class EntityModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		install(new FactoryModuleBuilder().implement(Agent.class, AgentEntity.class).implement(Attribute.class, AttributeEntity.class)
-				.implement(Capability.class, CapabilityEntity.class).implement(Characteristic.class, CharacteristicEntity.class)
-				.implement(InstanceGoal.class, InstanceGoalEntity.class).implement(Pmf.class, PmfEntity.class).implement(Policy.class, PolicyEntity.class)
-				.implement(Role.class, RoleEntity.class).implement(SpecificationGoal.class, SpecificationGoalEntity.class).build(EntityFactory.class));
+		install(new IdModule());
+		final FactoryModuleBuilder factoryModuleBuilder = new FactoryModuleBuilder();
+		install(factoryModuleBuilder.implement(Agent.class, AgentEntity.class).build(AgentFactory.class));
+		install(factoryModuleBuilder.implement(Attribute.class, AttributeEntity.class).build(AttributeFactory.class));
+		install(factoryModuleBuilder.implement(Capability.class, CapabilityEntity.class).build(CapabilityFactory.class));
+		install(factoryModuleBuilder.implement(Characteristic.class, CharacteristicEntity.class).build(CharacteristicFactory.class));
+		install(factoryModuleBuilder.implement(InstanceGoal.class, InstanceGoalEntity.class).build(InstanceGoalFactory.class));
+		install(factoryModuleBuilder.implement(Pmf.class, PmfEntity.class).build(PmfFactory.class));
+		install(factoryModuleBuilder.implement(Policy.class, PolicyEntity.class).build(PolicyFactory.class));
+		install(factoryModuleBuilder.implement(Role.class, RoleEntity.class).build(RoleFactory.class));
+		install(factoryModuleBuilder.implement(SpecificationGoal.class, SpecificationGoalEntity.class).build(SpecificationGoalFactory.class));
+		install(factoryModuleBuilder.build(EntityFactory.class));
 	}
 
 }
---
+++ src/main/java/model/organization/entity/RoleFactory.java
@@ -0,0 +1,23 @@
+package model.organization.entity;
+
+import model.organization.id.UniqueId;
+
+/**
+ * The {@linkplain RoleFactory} interface defines the APIs for constructing {@linkplain Role}s.
+ *
+ * @author Christopher Zhong
+ * @since 7.0.0
+ */
+@FunctionalInterface
+public interface RoleFactory {
+
+	/**
+	 * Constructs a {@linkplain Role}.
+	 *
+	 * @param id
+	 *            the {@linkplain UniqueId} that represents the {@linkplain Role}.
+	 * @return a {@linkplain Role}.
+	 */
+	Role buildRole(UniqueId<Role> id);
+
+}
