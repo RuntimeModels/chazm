ff95ad40e70c45ef9067e6182a09505cc3d1d4a7
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-18 12:31:36 +0800

    extracted interfaces from EntityFactory so that each entity has their own factory (i.e. AgentFactory, etc)

---
+++ src/main/java/model/organization/entity/PolicyFactory.java
@@ -0,0 +1,23 @@
+package model.organization.entity;
+
+import model.organization.id.UniqueId;
+
+/**
+ * The {@linkplain PolicyFactory} interface defines the APIs for constructing {@linkplain Policy}s.
+ *
+ * @author Christopher Zhong
+ * @since 7.0.0
+ */
+@FunctionalInterface
+public interface PolicyFactory {
+
+	/**
+	 * Constructs a {@linkplain Policy}.
+	 *
+	 * @param id
+	 *            the {@linkplain UniqueId} that represents the {@linkplain Policy}.
+	 * @return a {@linkplain Agent}.
+	 */
+	Policy buildPolicy(UniqueId<Policy> id);
+
+}
