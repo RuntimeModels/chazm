8bdc2088ac96e2497cb1b2da83b09cd85db36305
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-18 12:51:53 +0800

    goodness and effectiveness that were removed from role and pmf are added to organization

--- src/api/java/model/organization/Organization.java
+++ src/api/java/model/organization/Organization.java
@@ -1,14 +1,9 @@
-/*
- * Organization.java
- *
- * Created on Mar 24, 2009
- *
- * See License.txt file the license agreement.
- */
 package model.organization;
 
+import java.util.Collection;
 import java.util.function.Predicate;
 
+import model.organization.entity.Role;
 import model.organization.entity.manager.AgentManager;
 import model.organization.entity.manager.AttributeManager;
 import model.organization.entity.manager.CapabilityManager;
@@ -18,6 +13,9 @@ import model.organization.entity.manager.PmfManager;
 import model.organization.entity.manager.PolicyManager;
 import model.organization.entity.manager.RoleManager;
 import model.organization.entity.manager.SpecificationGoalManager;
+import model.organization.function.Goodness;
+import model.organization.id.UniqueId;
+import model.organization.relation.Assignment;
 import model.organization.relation.manager.AchievesManager;
 import model.organization.relation.manager.AssignmentManager;
 import model.organization.relation.manager.ContainsManager;
@@ -37,6 +35,7 @@ import model.organization.relation.manager.UsesManager;
 public interface Organization extends SpecificationGoalManager, RoleManager, AgentManager, CapabilityManager, PolicyManager, InstanceGoalManager,
 		AttributeManager, PmfManager, CharacteristicManager, AssignmentManager, AchievesManager, RequiresManager, PossessesManager, NeedsManager, HasManager,
 		UsesManager, ModeratesManager, ContainsManager {
+
 	/**
 	 * Checks if this {@linkplain Organization} is valid. Validity rules differ from one organization to another, so there is no general algorithm to determine
 	 * the validity of an {@linkplain Organization}.
@@ -48,4 +47,33 @@ public interface Organization extends SpecificationGoalManager, RoleManager, Age
 	default boolean isValid(final Predicate<Organization> p) {
 		return p.test(this);
 	}
+
+	/**
+	 * Returns a <code>double</code>-valued score of this {@linkplain Organization}'s effectiveness.
+	 *
+	 * @param assignments
+	 *            a set of {@linkplain Assignment}s.
+	 * @return a <code>double</code>-valued score.
+	 */
+	double effectiveness(Collection<Assignment> assignments);
+
+	/**
+	 * Returns the {@linkplain Goodness} function associated with a {@linkplain Role} from this {@linkplain Organization}.
+	 *
+	 * @param id
+	 *            the {@linkplain UniqueId} that represents a {@linkplain Role}.
+	 * @return a {@linkplain Goodness} function.
+	 */
+	Goodness getGoodness(UniqueId<Role> id);
+
+	/**
+	 * Sets a new {@linkplain Goodness} function for a {@linkplain Role} in this {@linkplain Organization}.
+	 *
+	 * @param id
+	 *            the {@linkplain UniqueId} that represents a {@linkplain Role}.
+	 * @param goodness
+	 *            the {@linkplain Goodness} function.
+	 */
+	void setGoodness(UniqueId<Role> id, Goodness goodness);
+
 }
