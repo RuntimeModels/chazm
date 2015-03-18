0e8002b0b5299e9cff397372dedeb66fce59475f
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-18 12:50:56 +0800

    updated test cases, added some suites so that all tests in a package can be tested

---
+++ src/test/java/model/organization/id/AllTests.java
@@ -0,0 +1,10 @@
+package model.organization.id;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuppressWarnings("javadoc")
+@RunWith(Suite.class)
+@SuiteClasses({ ClassIdTest.class, LongIdTest.class, StringIdTest.class })
+public class AllTests {}
