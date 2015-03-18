fecb26b650cd1a4526a8f0423da92f7937005b2d
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-18 12:26:20 +0800

    added a profiling module, an aspect to track method execution time and a @DoNotProfile annotation

---
+++ src/main/java/model/organization/profiling/DoNotProfile.java
@@ -0,0 +1,16 @@
+package model.organization.profiling;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @author Christopher Zhong
+ * @since 7.0.0
+ */
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface DoNotProfile {}
