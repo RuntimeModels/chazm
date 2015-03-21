624f9f8822823544216b1e951c78b0dbdc08a1b2
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2015-03-22 02:05:17 +0800

    corrected missing s from %s

--- src/main/java/message/M.java
+++ src/main/java/message/M.java
@@ -4,5 +4,5 @@ package message;
 public final class M {
 	public static final String EXCEPTION_PARAMETER_CANNOT_BE_NULL = "Parameter (%s) cannot be null";
 	public static final String EXCEPTION_ENTITY_DOES_NOT_EXISTS = "(%s) entity (%s) does not exists";
-	public static final String EXCEPTION_ENTITY_EXISTS = "(%) entity (%s) already exists";
+	public static final String EXCEPTION_ENTITY_EXISTS = "(%s) entity (%s) already exists";
 }
