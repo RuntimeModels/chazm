0a06afdb1a91f825706e06057b6ae7c691c81599
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-25 23:32:51 +0800

    added junit test cases for NeedsEvent; added code for missing equals(), hashCode(), and toString() methods; swap parameter order for NeedsEventconstructor and NeedsEventFactory

--- src/main/java/model/organization/event/NeedsEvent.java
+++ src/main/java/model/organization/event/NeedsEvent.java
@@ -23,9 +23,11 @@ public class NeedsEvent extends AbstractEvent {
 	private static final long serialVersionUID = -2368504360543452399L;
 	private final UniqueId<Role> roleId;
 	private final UniqueId<Attribute> attributeId;
+	private transient Integer hashCode = null;
+	private transient String toString = null;
 
 	@Inject
-	NeedsEvent(@NotNull @Assisted final Needs needs, @NotNull @Assisted final EventCategory category) {
+	NeedsEvent(@NotNull @Assisted final EventCategory category, @NotNull @Assisted final Needs needs) {
 		super(category);
 		checkNotNull(needs, "needs");
 		roleId = needs.getRole().getId();
@@ -48,6 +50,34 @@ public class NeedsEvent extends AbstractEvent {
 	 */
 	public UniqueId<Attribute> getAttributeId() {
 		return attributeId;
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		if (object instanceof NeedsEvent) {
+			final NeedsEvent event = (NeedsEvent) object;
+			return super.equals(event) && getRoleId().equals(event.getRoleId()) && getAttributeId().equals(event.getAttributeId());
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		if (hashCode == null) {
+			final int prime = 31;
+			hashCode = super.hashCode();
+			hashCode = prime * hashCode + getRoleId().hashCode();
+			hashCode = prime * hashCode + getAttributeId().hashCode();
+		}
+		return hashCode;
+	}
+
+	@Override
+	public String toString() {
+		if (toString == null) {
+			toString = String.format("%s(%s, %s, %s)", getClass().getSimpleName(), getCategory(), getRoleId(), getAttributeId());
+		}
+		return toString;
 	}
 
 }
