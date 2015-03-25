2eb82c56cfb0fb3f1d543dacf0e92b690f9a9a5a
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-03-25 23:54:51 +0800

    added junit test cases for UsesEvent; added code for missing equals(), hashCode(), and toString() methods; swap parameter order for UsesEventconstructor and UsesEventFactory

--- src/main/java/model/organization/event/UsesEvent.java
+++ src/main/java/model/organization/event/UsesEvent.java
@@ -23,9 +23,11 @@ public class UsesEvent extends AbstractEvent {
 	private static final long serialVersionUID = 5013118334434822084L;
 	private final UniqueId<Role> roleId;
 	private final UniqueId<Pmf> pmfId;
+	private transient Integer hashCode = null;
+	private transient String toString = null;
 
 	@Inject
-	UsesEvent(@NotNull @Assisted final Uses uses, @NotNull @Assisted final EventCategory category) {
+	UsesEvent(@NotNull @Assisted final EventCategory category, @NotNull @Assisted final Uses uses) {
 		super(category);
 		checkNotNull(uses, "uses");
 		roleId = uses.getRole().getId();
@@ -48,6 +50,34 @@ public class UsesEvent extends AbstractEvent {
 	 */
 	public UniqueId<Pmf> getPmfId() {
 		return pmfId;
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		if (object instanceof UsesEvent) {
+			final UsesEvent event = (UsesEvent) object;
+			return super.equals(event) && getRoleId().equals(event.getRoleId()) && getPmfId().equals(event.getPmfId());
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		if (hashCode == null) {
+			final int prime = 31;
+			hashCode = super.hashCode();
+			hashCode = prime * hashCode + getRoleId().hashCode();
+			hashCode = prime * hashCode + getPmfId().hashCode();
+		}
+		return hashCode;
+	}
+
+	@Override
+	public String toString() {
+		if (toString == null) {
+			toString = String.format("%s(%s, %s, %s)", getClass().getSimpleName(), getCategory(), getRoleId(), getPmfId());
+		}
+		return toString;
 	}
 
 }
