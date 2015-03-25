7dbc7e1c9b433c9804c4d439469f9b0a225c0174
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2015-03-25 23:25:00 +0800

    added junit test cases for ModeratesEvent; added code for missing equals(), hashCode(), and toString() methods; swap parameter order for ModeratesEventconstructor and ModeratesEventFactory

--- src/main/java/model/organization/event/ModeratesEvent.java
+++ src/main/java/model/organization/event/ModeratesEvent.java
@@ -23,11 +23,13 @@ public class ModeratesEvent extends AbstractEvent {
 	private static final long serialVersionUID = 273935856408749575L;
 	private final UniqueId<Pmf> pmfId;
 	private final UniqueId<Attribute> attributeId;
+	private transient Integer hashCode = null;
+	private transient String toString = null;
 
 	@Inject
-	ModeratesEvent(@NotNull @Assisted final Moderates moderates, @NotNull @Assisted final EventCategory category) {
+	ModeratesEvent(@NotNull @Assisted final EventCategory category, @NotNull @Assisted final Moderates moderates) {
 		super(category);
-		checkNotNull(moderates, "agent");
+		checkNotNull(moderates, "moderates");
 		pmfId = moderates.getPmf().getId();
 		attributeId = moderates.getAttribute().getId();
 	}
@@ -48,6 +50,34 @@ public class ModeratesEvent extends AbstractEvent {
 	 */
 	public UniqueId<Attribute> getAttributeId() {
 		return attributeId;
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		if (object instanceof ModeratesEvent) {
+			final ModeratesEvent event = (ModeratesEvent) object;
+			return super.equals(event) && getPmfId().equals(event.getPmfId()) && getAttributeId().equals(event.getAttributeId());
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		if (hashCode == null) {
+			final int prime = 31;
+			hashCode = super.hashCode();
+			hashCode = prime * hashCode + getPmfId().hashCode();
+			hashCode = prime * hashCode + getAttributeId().hashCode();
+		}
+		return hashCode;
+	}
+
+	@Override
+	public String toString() {
+		if (toString == null) {
+			toString = String.format("%s(%s, %s, %s)", getClass().getSimpleName(), getCategory(), getPmfId(), getAttributeId());
+		}
+		return toString;
 	}
 
 }
