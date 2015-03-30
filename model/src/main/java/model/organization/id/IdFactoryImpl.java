f52ed41164e19e62d96163361947a99e10c2a1fb
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2015-03-30 19:18:26 +0800

    removed @NotNull from IdFactoryImpl as it is not needed

--- model/src/main/java/model/organization/id/IdFactoryImpl.java
+++ model/src/main/java/model/organization/id/IdFactoryImpl.java
@@ -1,24 +1,22 @@
 package model.organization.id;
 
-import javax.validation.constraints.NotNull;
-
 import aop.profiling.DoNotProfile;
 
 @DoNotProfile
 class IdFactoryImpl implements IdFactory {
 
 	@Override
-	public <T, U> UniqueId<T> build(@NotNull final Class<T> clazz, @NotNull final Class<U> id) {
+	public <T, U> UniqueId<T> build(final Class<T> clazz, final Class<U> id) {
 		return new ClassId<>(clazz, id);
 	}
 
 	@Override
-	public <T> UniqueId<T> build(@NotNull final Class<T> clazz, @NotNull final Long id) {
+	public <T> UniqueId<T> build(final Class<T> clazz, final Long id) {
 		return new LongId<>(clazz, id);
 	}
 
 	@Override
-	public <T> StringId<T> build(@NotNull final Class<T> clazz, @NotNull final String id) {
+	public <T> StringId<T> build(final Class<T> clazz, final String id) {
 		return new StringId<>(clazz, id);
 	}
 
