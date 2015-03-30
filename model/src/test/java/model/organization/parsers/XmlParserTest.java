984cb57f984ae35a33bd104f0151b6b14672ff4b
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2015-03-30 18:46:13 +0800

    added junit tests for XmlParser

---
+++ model/src/test/java/model/organization/parsers/XmlParserTest.java
@@ -0,0 +1,51 @@
+package model.organization.parsers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
+import java.io.InputStream;
+
+import javax.inject.Provider;
+import javax.xml.stream.XMLStreamException;
+
+import model.organization.Organization;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+@SuppressWarnings("javadoc")
+public class XmlParserTest {
+
+	private final Injector injector = Guice.createInjector(new ParsersModule());
+	private final Provider<XmlParser> provider = injector.getProvider(XmlParser.class);
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	@Test
+	public void testXmlParser() throws XMLStreamException {
+		XmlParser p1 = provider.get();
+		XmlParser p2 = provider.get();
+
+		assertThat(p1, is(not(nullValue())));
+		assertThat(p1, is(sameInstance(p2)));
+	}
+
+	@Test
+	public void testParse() throws XMLStreamException {
+		XmlParser p1 = provider.get();
+		Organization o1 = injector.getInstance(Organization.class);
+		InputStream is = ClassLoader.getSystemResourceAsStream("RoleDiagram1.xml");
+
+		p1.parse(o1, is);
+
+	}
+
+}
