485f1455291e0a92dc5fa8a3a88ba2be083a42bc
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2015-04-02 22:10:43 +0800

    updated XML parser code

--- model/src/main/java/message/E.java
+++ model/src/main/java/message/E.java
@@ -5,6 +5,7 @@ public enum E { // for exceptions
 
 	ENTITY_ALREADY_EXISTS("(%s) entity (%s) already exists"), //
 	ENTITY_DOES_NOT_EXISTS("(%s) entity (%s) does not exists"), //
+	MISSING_END_TAG("Tag (<%1$s>) is missing enclosing tag </ %1$s>"), //
 	PARAMETER_CANNOT_BE_NULL("Parameter (%s) cannot be null"), //
 	SCORE_BETWEEN("Score (%s) must be between (%s) and (%s)"), //
 	VALUE_AT_LEAST("For atttribute type (%s), value (%s) must be at least (%s)"), //
