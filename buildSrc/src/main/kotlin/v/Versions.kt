181b03f81e71406f364ffa4da3b55d2c2bec349f
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-09-17 19:31:06 +0800

    #42: add more versioning to buildSrc
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- buildSrc/src/main/kotlin/v/Versions.kt
+++ buildSrc/src/main/kotlin/v/Versions.kt
@@ -3,3 +3,7 @@ package v
 const val gradle = "4.10.1"
 const val kotlin = "1.2.70"
 const val bintray = "1.8.4"
+const val springBoot = "2.0.5.RELEASE"
+const val guice = "4.2.0"
+const val lombok = "1.18.2"
+const val junit = "5.3.1"
