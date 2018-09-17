df0531e3f6d3d3a59d302fc4f1340040cb61c682
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-09-17 17:57:29 +0800

    #42: add buildSrc directory
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ buildSrc/src/main/kotlin/v/Versions.kt
@@ -0,0 +1,5 @@
+package v
+
+const val gradle = "4.10.1"
+const val kotlin = "1.2.70"
+const val bintray = "1.8.4"
--- chazm-api/build.gradle.kts
+++ chazm-api/build.gradle.kts
@@ -1,15 +1,11 @@
+import java.time.Instant
+
 plugins {
-    java
+    `java-library`
     jacoco
     `maven-publish`
-}
-
-group = rootProject.group
-version = "${rootProject.version}.0.0"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_10
-    targetCompatibility = JavaVersion.VERSION_1_10
+    signing
+    id("com.jfrog.bintray")
 }
 
 repositories {
@@ -17,13 +13,59 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_10
+    targetCompatibility = JavaVersion.VERSION_1_10
+}
+
+group = rootProject.group
+version = "${rootProject.version}.0.0"
+
 dependencies {
+}
+
+val sourceJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets["main"].allJava)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourceJar.get())
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    setPublications("maven")
+    dryRun = System.getenv("TRAVIS_BRANCH") != "master" || System.getenv("TRAVIS_PULL_REQUEST") != "false"
+    publish = System.getenv("TRAVIS_BRANCH") == "master" && System.getenv("TRAVIS_PULL_REQUEST") == "false"
+
+    pkg.repo = "maven"
+    pkg.name = "chazm"
+    pkg.userOrg = "runtimemodels"
+    pkg.desc = "An organizational runtime model"
+    pkg.websiteUrl = "https://runtimemodels.github.io/chazm/"
+    pkg.issueTrackerUrl = "https://github.com/runtimemodels/chazm/issues"
+    pkg.vcsUrl = "https://github.com/RuntimeModels/chazm.git"
+    pkg.setLicenses("Apache-2.0")
+    pkg.githubRepo = "RuntimeModels/chazm"
+    pkg.githubReleaseNotesFile = "CHANGES.md"
+    pkg.version.name = "${project.version}"
+    pkg.version.desc = "Chazm"
+    pkg.version.released = Instant.now().toString()
 }
 
 tasks {
     withType(JacocoReport::class) {
         reports {
+            csv.isEnabled = false
             xml.isEnabled = true
+            html.isEnabled = System.getenv("CI").isNullOrBlank()
         }
     }
 }
