b6b7a337c58fd20e0e71f1d58762878d95cf64a5
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-09-18 23:50:33 +0800

    #47: add a test case to `chazm-api` to test the default method
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- buildSrc/src/main/kotlin/d/Dependencies.kt
+++ buildSrc/src/main/kotlin/d/Dependencies.kt
@@ -1,20 +1,15 @@
-package library
-
-import v.guice
-import v.kotlin
-import v.springBoot
-
+package d
 
 object kotlin {
-    val jvm = "org.jetbrains.kotlin.jvm:$kotlin"
+    val jvm = "org.jetbrains.kotlin.jvm:${v.kotlin}"
 }
 
 object springBoot {
-    val bom = "org.springframework.boot:spring-boot-starter-parent:$springBoot"
+    val bom = "org.springframework.boot:spring-boot-starter-parent:${v.springBoot}"
 }
 
 object guice {
-    val bom = "com.google.inject:guice-bom:$guice"
+    val bom = "com.google.inject:guice-bom:${v.guice}"
     val core = "com.google.inject:guice"
     val `assisted-inject` = "com.google.inject.extensions:guice-assistedinject"
 }
@@ -23,10 +18,14 @@ val lombok = "org.projectlombok:lombok:${v.lombok}"
 
 object junit {
     val bom = "org.junit:junit-bom:${v.junit}"
+
     object jupiter {
         val api = "org.junit.jupiter:junit-jupiter-api"
         val engine = "org.junit.jupiter:junit-jupiter-engine"
         val params = "org.junit.jupiter:junit-jupiter-params"
     }
+
     val vintage = "org.junit.vintage:junit-vintage-engine"
 }
+
+val `assertj-core` = "org.assertj:assertj-core:${v.assertj}"
