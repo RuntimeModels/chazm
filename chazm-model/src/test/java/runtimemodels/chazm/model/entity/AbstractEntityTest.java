23580a1fa53b508d94a6fed2521d003dd32ee7d1
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-17 16:47:02 +0800

    #54: convert AbstractEntityTest to use JUnit 5
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/src/test/java/runtimemodels/chazm/model/entity/AbstractEntityTest.java
+++ chazm-model/src/test/java/runtimemodels/chazm/model/entity/AbstractEntityTest.java
@@ -2,25 +2,19 @@ package runtimemodels.chazm.model.entity;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 import runtimemodels.chazm.api.entity.Agent;
 import runtimemodels.chazm.api.id.UniqueId;
 import runtimemodels.chazm.model.id.IdFactory;
 import runtimemodels.chazm.model.id.IdModule;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@SuppressWarnings("javadoc")
 public class AbstractEntityTest {
 
     private final Injector injector = Guice.createInjector(new IdModule());
     private final IdFactory idf = injector.getInstance(IdFactory.class);
-
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void testAbstractEntity() {
@@ -30,8 +24,11 @@ public class AbstractEntityTest {
         final AbstractEntity<Agent> e2 = new AbstractEntity<Agent>(i1) {
         };
 
-        assertThat(e1, is(not(nullValue())));
-        assertThat(e1, is(not(sameInstance(e2))));
+        assertAll(
+                () -> assertThat(e1).isNotNull(),
+                () -> assertThat(e2).isNotNull(),
+                () -> assertThat(e1).isNotSameAs(e2)
+        );
     }
 
     @Test
@@ -42,8 +39,10 @@ public class AbstractEntityTest {
         final AbstractEntity<Agent> e2 = new AbstractEntity<Agent>(i1) {
         };
 
-        assertThat(e1.getId(), is(sameInstance(i1)));
-        assertThat(e2.getId(), is(sameInstance(i1)));
+        assertAll(
+                () -> assertThat(e1.getId()).isSameAs(i1),
+                () -> assertThat(e2.getId()).isSameAs(i1)
+        );
     }
 
     @Test
@@ -57,9 +56,11 @@ public class AbstractEntityTest {
         final AbstractEntity<Agent> e3 = new AbstractEntity<Agent>(i1) {
         };
 
-        assertThat(e1, is(not(equalTo(e2))));
-        assertThat(e1, is(equalTo(e3)));
-        assertThat(e1, is(not(equalTo(""))));
+        assertAll(
+                () -> assertThat(e1).isNotEqualTo(e2),
+                () -> assertThat(e1).isEqualTo(e3),
+                () -> assertThat(e1).isNotEqualTo("")
+        );
     }
 
     @Test
@@ -73,8 +74,10 @@ public class AbstractEntityTest {
         final AbstractEntity<Agent> e3 = new AbstractEntity<Agent>(i1) {
         };
 
-        assertThat(e1.hashCode(), is(not(equalTo(e2.hashCode()))));
-        assertThat(e1.hashCode(), is(equalTo(e3.hashCode())));
+        assertAll(
+                () -> assertThat(e1.hashCode()).isNotEqualTo(e2.hashCode()),
+                () -> assertThat(e1.hashCode()).isEqualTo(e3.hashCode())
+        );
     }
 
     @Test
@@ -88,8 +91,10 @@ public class AbstractEntityTest {
         final AbstractEntity<Agent> e3 = new AbstractEntity<Agent>(i1) {
         };
 
-        assertThat(e1.toString(), is(not(equalTo(e2.toString()))));
-        assertThat(e1.toString(), is(equalTo(e3.toString())));
+        assertAll(
+                () -> assertThat(e1.toString()).isNotEqualTo(e2.toString()),
+                () -> assertThat(e1.toString()).isEqualTo(e3.toString())
+        );
     }
 
 }
