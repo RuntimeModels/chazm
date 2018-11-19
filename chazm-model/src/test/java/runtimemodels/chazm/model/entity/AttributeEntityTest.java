ecf2952ebcce1eb34d5bd77fe8e92e3eb0c833bd
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-19 10:30:33 +0800

    #54: convert AttributeEntityTest to use JUnit 5
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/src/test/java/runtimemodels/chazm/model/entity/AttributeEntityTest.java
+++ chazm-model/src/test/java/runtimemodels/chazm/model/entity/AttributeEntityTest.java
@@ -3,54 +3,45 @@ package runtimemodels.chazm.model.entity;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.ProvisionException;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 import runtimemodels.chazm.api.entity.Attribute;
 import runtimemodels.chazm.api.id.UniqueId;
 import runtimemodels.chazm.model.id.IdFactory;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SuppressWarnings("javadoc")
 public class AttributeEntityTest {
 
     private final Injector injector = Guice.createInjector(new EntityModule());
     private final AttributeFactory attributeFactory = injector.getInstance(AttributeFactory.class);
     private final IdFactory idFactory = injector.getInstance(IdFactory.class);
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void testAttributeFactory() {
         final UniqueId<Attribute> i1 = idFactory.build(Attribute.class, 1L);
         final Attribute a1 = attributeFactory.buildAttribute(i1, Attribute.Type.NEGATIVE_QUALITY);
         final Attribute a2 = attributeFactory.buildAttribute(i1, Attribute.Type.NEGATIVE_QUALITY);
-        assertThat(a1, is(not(nullValue())));
-        assertThat(a1, is(not(sameInstance(a2))));
+
+        assertAll(
+                () -> assertThat(a1).isNotNull(),
+                () -> assertThat(a2).isNotNull(),
+                () -> assertThat(a1).isNotSameAs(a2)
+        );
     }
 
     @Test
-    @Ignore
     public void testAttributeFactoryWithNullIdAndNullType() {
-        exception.expect(instanceOf(ProvisionException.class));
-//        exception.expectMessage(allOf(
-//                containsString("1st parameter of AttributeEntity.<init>(AttributeEntity.java:21) is not @Nullable"),
-//                containsString("2nd parameter of AttributeEntity.<init>(AttributeEntity.java:21) is not @Nullable")
-//        ));
-        attributeFactory.buildAttribute(null, null);
+        assertThrows(ProvisionException.class, () -> attributeFactory.buildAttribute(null, null));
     }
 
     @Test
-    @Ignore
     public void testAttributeFactoryWithNullType() {
         final UniqueId<Attribute> i1 = idFactory.build(Attribute.class, 1L);
-        exception.expect(instanceOf(ProvisionException.class));
-//        exception.expectMessage(containsString("2nd parameter of AttributeEntity.<init>(AttributeEntity.java:21) is not @Nullable"));
-        attributeFactory.buildAttribute(i1, null);
+
+        assertThrows(ProvisionException.class, () -> attributeFactory.buildAttribute(i1, null));
     }
 
     @Test
@@ -62,12 +53,15 @@ public class AttributeEntityTest {
         final Attribute a4 = attributeFactory.buildAttribute(i1, Attribute.Type.POSITIVE_QUALITY);
         final Attribute a5 = attributeFactory.buildAttribute(i1, Attribute.Type.POSITIVE_QUANTITY);
         final Attribute a6 = attributeFactory.buildAttribute(i1, Attribute.Type.POSITIVE_UNBOUNDED);
-        assertThat(a1.getType(), is(equalTo(Attribute.Type.NEGATIVE_QUALITY)));
-        assertThat(a2.getType(), is(equalTo(Attribute.Type.NEGATIVE_QUANTITY)));
-        assertThat(a3.getType(), is(equalTo(Attribute.Type.NEGATIVE_UNBOUNDED)));
-        assertThat(a4.getType(), is(equalTo(Attribute.Type.POSITIVE_QUALITY)));
-        assertThat(a5.getType(), is(equalTo(Attribute.Type.POSITIVE_QUANTITY)));
-        assertThat(a6.getType(), is(equalTo(Attribute.Type.POSITIVE_UNBOUNDED)));
+
+        assertAll(
+                () -> assertThat(a1.getType()).isEqualTo(Attribute.Type.NEGATIVE_QUALITY),
+                () -> assertThat(a2.getType()).isEqualTo(Attribute.Type.NEGATIVE_QUANTITY),
+                () -> assertThat(a3.getType()).isEqualTo(Attribute.Type.NEGATIVE_UNBOUNDED),
+                () -> assertThat(a4.getType()).isEqualTo(Attribute.Type.POSITIVE_QUALITY),
+                () -> assertThat(a5.getType()).isEqualTo(Attribute.Type.POSITIVE_QUANTITY),
+                () -> assertThat(a6.getType()).isEqualTo(Attribute.Type.POSITIVE_UNBOUNDED)
+        );
     }
 
     @Test
@@ -77,8 +71,10 @@ public class AttributeEntityTest {
         final Attribute c1 = attributeFactory.buildAttribute(i1, Attribute.Type.NEGATIVE_QUALITY);
         final Attribute c2 = attributeFactory.buildAttribute(i2, Attribute.Type.NEGATIVE_QUALITY);
 
-        assertThat(c1.getId(), is(sameInstance(i1)));
-        assertThat(c1.getId(), is(not(sameInstance(c2.getId()))));
+        assertAll(
+                () -> assertThat(c1.getId()).isSameAs(i1),
+                () -> assertThat(c1.getId()).isNotSameAs(c2.getId())
+        );
     }
 
     @Test
@@ -89,9 +85,11 @@ public class AttributeEntityTest {
         final Attribute c2 = attributeFactory.buildAttribute(i2, Attribute.Type.NEGATIVE_QUALITY);
         final Attribute c3 = attributeFactory.buildAttribute(i1, Attribute.Type.NEGATIVE_QUALITY);
 
-        assertThat(c1, is(not(equalTo(c2))));
-        assertThat(c1, is(equalTo(c3)));
-        assertThat(c1, is(not(equalTo(""))));
+        assertAll(
+                () -> assertThat(c1).isNotEqualTo(c2),
+                () -> assertThat(c1).isEqualTo(c3),
+                () -> assertThat(c1).isNotEqualTo("")
+        );
     }
 
     @Test
@@ -102,8 +100,10 @@ public class AttributeEntityTest {
         final Attribute c2 = attributeFactory.buildAttribute(i2, Attribute.Type.NEGATIVE_QUALITY);
         final Attribute c3 = attributeFactory.buildAttribute(i1, Attribute.Type.NEGATIVE_QUALITY);
 
-        assertThat(c1.hashCode(), is(not(equalTo(c2.hashCode()))));
-        assertThat(c1.hashCode(), is(equalTo(c3.hashCode())));
+        assertAll(
+                () -> assertThat(c1.hashCode()).isNotEqualTo(c2.hashCode()),
+                () -> assertThat(c1.hashCode()).isEqualTo(c3.hashCode())
+        );
     }
 
     @Test
@@ -114,8 +114,10 @@ public class AttributeEntityTest {
         final Attribute c2 = attributeFactory.buildAttribute(i2, Attribute.Type.NEGATIVE_QUALITY);
         final Attribute c3 = attributeFactory.buildAttribute(i1, Attribute.Type.NEGATIVE_QUALITY);
 
-        assertThat(c1.toString(), is(not(equalTo(c2.toString()))));
-        assertThat(c1.toString(), is(equalTo(c3.toString())));
+        assertAll(
+                () -> assertThat(c1.toString()).isNotEqualTo(c2.toString()),
+                () -> assertThat(c1.toString()).isEqualTo(c3.toString())
+        );
     }
 
 }
