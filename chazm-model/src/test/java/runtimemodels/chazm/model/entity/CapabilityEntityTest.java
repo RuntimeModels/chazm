6b4eee419d1c27bb51f1a0ed424c651b9d0160a1
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-20 09:05:47 +0800

    #54: convert CapabilityEntityTest to use JUnit 5
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/src/test/java/runtimemodels/chazm/model/entity/CapabilityEntityTest.java
+++ chazm-model/src/test/java/runtimemodels/chazm/model/entity/CapabilityEntityTest.java
@@ -3,26 +3,22 @@ package runtimemodels.chazm.model.entity;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.ProvisionException;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 import runtimemodels.chazm.api.entity.Capability;
 import runtimemodels.chazm.api.id.UniqueId;
 import runtimemodels.chazm.model.id.IdFactory;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SuppressWarnings("javadoc")
+
 public class CapabilityEntityTest {
 
     private final Injector injector = Guice.createInjector(new EntityModule());
     private final CapabilityFactory capabilityFactory = injector.getInstance(CapabilityFactory.class);
     private final IdFactory idFactory = injector.getInstance(IdFactory.class);
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void testCapabilityFactory() {
@@ -30,16 +26,16 @@ public class CapabilityEntityTest {
         final Capability c1 = capabilityFactory.buildCapability(i1);
         final Capability c2 = capabilityFactory.buildCapability(i1);
 
-        assertThat(c1, is(not(nullValue())));
-        assertThat(c1, is(not(sameInstance(c2))));
+        assertAll(
+                () -> assertThat(c1).isNotNull(),
+                () -> assertThat(c2).isNotNull(),
+                () -> assertThat(c1).isNotSameAs(c2)
+        );
     }
 
     @Test
-    @Ignore
     public void testCapabilityFactoryWithNullId() {
-        exception.expect(instanceOf(ProvisionException.class));
-//        exception.expectMessage(containsString("1st parameter of CapabilityEntity.<init>(CapabilityEntity.java:13) is not @Nullable"));
-        capabilityFactory.buildCapability(null);
+        assertThrows(ProvisionException.class, () -> capabilityFactory.buildCapability(null));
     }
 
     @Test
@@ -49,8 +45,10 @@ public class CapabilityEntityTest {
         final Capability c1 = capabilityFactory.buildCapability(i1);
         final Capability c2 = capabilityFactory.buildCapability(i2);
 
-        assertThat(c1.getId(), is(sameInstance(i1)));
-        assertThat(c1.getId(), is(not(sameInstance(c2.getId()))));
+        assertAll(
+                () -> assertThat(c1.getId()).isSameAs(i1),
+                () -> assertThat(c1.getId()).isNotSameAs(c2.getId())
+        );
     }
 
     @Test
@@ -61,9 +59,11 @@ public class CapabilityEntityTest {
         final Capability c2 = capabilityFactory.buildCapability(i2);
         final Capability c3 = capabilityFactory.buildCapability(i1);
 
-        assertThat(c1, is(not(equalTo(c2))));
-        assertThat(c1, is(equalTo(c3)));
-        assertThat(c1, is(not(equalTo(""))));
+        assertAll(
+                () -> assertThat(c1).isNotEqualTo(c2),
+                () -> assertThat(c1).isEqualTo(c3),
+                () -> assertThat(c1).isNotEqualTo("")
+        );
     }
 
     @Test
@@ -74,8 +74,10 @@ public class CapabilityEntityTest {
         final Capability c2 = capabilityFactory.buildCapability(i2);
         final Capability c3 = capabilityFactory.buildCapability(i1);
 
-        assertThat(c1.hashCode(), is(not(equalTo(c2.hashCode()))));
-        assertThat(c1.hashCode(), is(equalTo(c3.hashCode())));
+        assertAll(
+                () -> assertThat(c1.hashCode()).isNotEqualTo(c2.hashCode()),
+                () -> assertThat(c1.hashCode()).isEqualTo(c3.hashCode())
+        );
     }
 
     @Test
@@ -86,8 +88,10 @@ public class CapabilityEntityTest {
         final Capability c2 = capabilityFactory.buildCapability(i2);
         final Capability c3 = capabilityFactory.buildCapability(i1);
 
-        assertThat(c1.toString(), is(not(equalTo(c2.toString()))));
-        assertThat(c1.toString(), is(equalTo(c3.toString())));
+        assertAll(
+                () -> assertThat(c1.toString()).isNotEqualTo(c2.toString()),
+                () -> assertThat(c1.toString()).isEqualTo(c3.toString())
+        );
     }
 
 }
