35f580303d4ede401ba4e3fd7a5d141c23e855e7
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-23 07:33:31 +0800

    #56: change source and target version to Java 11
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/build.gradle.kts
+++ chazm-model/build.gradle.kts
@@ -19,8 +19,8 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_10
-    targetCompatibility = JavaVersion.VERSION_1_10
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 group = rootProject.group
