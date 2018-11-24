cf13c650b4330d06502fc2dc7c2c286c64acea14
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-24 22:30:32 +0800

    #56: convert M from Java to Kotlin
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ chazm-model/src/main/java/runtimemodels/chazm/model/message/M.kt
@@ -0,0 +1,29 @@
+package runtimemodels.chazm.model.message
+
+import java.util.*
+
+enum class M(private val string: String) { // everything else
+
+    ASSIGNMENT("<%s, %s, %s>"), //
+    ASSIGNMENT_ID("%s(%s, %s, %s)"), //
+    ENTITY_0("%s(%s)"), //
+    ENTITY_1("%s: %s"), //
+    ENTITY_2("%s: %s, %s"), //
+    EVENT("%s(%s)"), //
+    EVENT_WITH_1_ID("%s: %s"), //
+    EVENT_WITH_2_IDS("%s: %s, %s"), //
+    EVENT_WITH_2_IDS_AND_VALUE("%s: %s, %s: %s"), //
+    EVENT_WITH_3_IDS("%s: %s, %s, %s"), //
+    RELATION("<%s, %s>"), //
+    RELATION_WITH_VALUE("<%s, %s>: %s");
+
+    operator fun get(vararg args: Any): String? {
+        return try {
+            String.format(string, *args)
+        } catch (e: MissingFormatArgumentException) {
+            null
+        }
+
+    }
+
+}
