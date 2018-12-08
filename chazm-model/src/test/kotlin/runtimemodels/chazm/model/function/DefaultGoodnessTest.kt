f6a71fff5ac61f27183b6caf50ab4e87ccbd3b04
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-08 16:03:11 +0800

    #12: add some test cases to DefaultGoodnessTest in preparation to remove Guice
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/src/test/kotlin/runtimemodels/chazm/model/function/DefaultGoodnessTest.kt
+++ chazm-model/src/test/kotlin/runtimemodels/chazm/model/function/DefaultGoodnessTest.kt
@@ -1,11 +1,19 @@
 package runtimemodels.chazm.model.function
 
+import any
 import com.google.inject.Guice
+import mock
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import runtimemodels.chazm.api.entity.Attribute
+import org.mockito.Mockito.`when`
+import runtimemodels.chazm.api.entity.*
 import runtimemodels.chazm.api.function.Goodness
-import runtimemodels.chazm.api.organization.Organization
+import runtimemodels.chazm.api.id.AttributeId
+import runtimemodels.chazm.api.id.RoleId
+import runtimemodels.chazm.api.id.SpecificationGoalId
+import runtimemodels.chazm.api.organization.*
+import runtimemodels.chazm.api.relation.Achieves
+import runtimemodels.chazm.api.relation.Needs
 import runtimemodels.chazm.model.factory.EntityFactory
 import runtimemodels.chazm.model.guice.OrganizationModule
 import runtimemodels.chazm.model.id.*
@@ -13,6 +21,7 @@ import runtimemodels.chazm.model.relation.AchievesRelation
 import runtimemodels.chazm.model.relation.NeedsRelation
 import runtimemodels.chazm.model.relation.PossessesRelation
 import runtimemodels.chazm.model.relation.RequiresRelation
+import scaleInt
 
 class DefaultGoodnessTest {
 
@@ -20,6 +29,81 @@ class DefaultGoodnessTest {
     private val provider = injector.getProvider(Organization::class.java)
     private val entityFactory = injector.getInstance(EntityFactory::class.java)
     private val goodness = injector.getInstance(Goodness::class.java)
+
+    @Test
+    fun `when the agent does not have the required attribute`() {
+        val organization: Organization = mock()
+        val agent: Agent = mock()
+        val role: Role = mock()
+        val goal: InstanceGoal = mock()
+        val roleId: RoleId = mock()
+        val needsRelations: NeedsManager = mock()
+        val attributeId: AttributeId = mock()
+        val needs: Needs = mock()
+        val hasRelations: HasManager = mock()
+
+        `when`(role.id).thenReturn(roleId)
+        `when`(organization.needsRelations).thenReturn(needsRelations)
+        `when`(needsRelations[roleId]).thenReturn(mapOf(attributeId to needs))
+        `when`(organization.hasRelations).thenReturn(hasRelations)
+        `when`(hasRelations[any(), any()]).thenReturn(null)
+
+        val goodness = DefaultGoodness()
+
+        assertThat(goodness.compute(organization, agent, role, goal, emptySet()).scaleInt()).isEqualTo(0)
+    }
+
+    @Test
+    fun `when the role does not achieve the goal`() {
+        val organization: Organization = mock()
+        val agent: Agent = mock()
+        val role: Role = mock()
+        val goal: InstanceGoal = mock()
+        val roleId: RoleId = mock()
+        val needsRelations: NeedsManager = mock()
+        val achievesRelations: AchievesManager = mock()
+        val specificationGoal: SpecificationGoal = mock()
+
+        `when`(role.id).thenReturn(roleId)
+        `when`(organization.needsRelations).thenReturn(needsRelations)
+        `when`(needsRelations[roleId]).thenReturn(emptyMap())
+        `when`(organization.achievesRelations).thenReturn(achievesRelations)
+        `when`(achievesRelations[roleId]).thenReturn(emptyMap())
+        `when`(goal.goal).thenReturn(specificationGoal)
+
+        val goodness = DefaultGoodness()
+
+        assertThat(goodness.compute(organization, agent, role, goal, emptySet()).scaleInt()).isEqualTo(0)
+    }
+
+    @Test
+    fun `when the role does not require any capability`() {
+        val organization: Organization = mock()
+        val agent: Agent = mock()
+        val role: Role = mock()
+        val goal: InstanceGoal = mock()
+        val roleId: RoleId = mock()
+        val needsRelations: NeedsManager = mock()
+        val achievesRelations: AchievesManager = mock()
+        val specificationGoal: SpecificationGoal = mock()
+        val specificationGoalId: SpecificationGoalId = mock()
+        val achieves: Achieves = mock()
+        val requiresRelation: RequiresManager = mock()
+
+        `when`(role.id).thenReturn(roleId)
+        `when`(organization.needsRelations).thenReturn(needsRelations)
+        `when`(needsRelations[roleId]).thenReturn(emptyMap())
+        `when`(organization.achievesRelations).thenReturn(achievesRelations)
+        `when`(achievesRelations[roleId]).thenReturn(mapOf(specificationGoalId to achieves))
+        `when`(goal.goal).thenReturn(specificationGoal)
+        `when`(specificationGoal.id).thenReturn(specificationGoalId)
+        `when`(organization.requiresRelations).thenReturn(requiresRelation)
+        `when`(requiresRelation[roleId]).thenReturn(emptyMap())
+
+        val goodness = DefaultGoodness()
+
+        assertThat(goodness.compute(organization, agent, role, goal, emptySet()).scaleInt()).isEqualTo(10)
+    }
 
     @Test
     fun `test that the compute function works properly`() {
