52ef8ff580e4ed52dffbee5820efdb57056ff376
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-09 16:23:18 +0800

    #12: group *Entity under a sealed class
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ chazm-model/src/main/kotlin/runtimemodels/chazm/model/entity/DefaultEntity.kt
@@ -0,0 +1,74 @@
+package runtimemodels.chazm.model.entity
+
+import com.google.inject.assistedinject.Assisted
+import runtimemodels.chazm.api.entity.*
+import runtimemodels.chazm.api.id.*
+import javax.inject.Inject
+
+sealed class DefaultEntity
+
+internal data class DefaultAgent @Inject constructor(
+    @Assisted override val id: AgentId
+) : Agent, DefaultEntity() {
+    internal data class DefaultContactInfo(
+        private val map: MutableMap<Any, Any> = mutableMapOf()
+    ) : Agent.ContactInfo, Map<Any, Any> by map {
+        override fun add(key: Any, value: Any) {
+            map[key] = value
+        }
+
+        override fun remove(key: Any) {
+            map.remove(key)
+        }
+    }
+
+    override val contactInfo: Agent.ContactInfo = DefaultContactInfo()
+}
+
+internal data class DefaultAttribute @Inject constructor(
+    @Assisted override val id: AttributeId,
+    @param:Assisted override val type: Attribute.Type
+) : Attribute, DefaultEntity()
+
+internal data class DefaultCapability @Inject constructor(
+    @Assisted override val id: CapabilityId
+) : Capability, DefaultEntity()
+
+internal data class DefaultCharacteristic @Inject constructor(
+    @Assisted override val id: CharacteristicId
+) : Characteristic, DefaultEntity()
+
+internal data class DefaultInstanceGoal @Inject constructor(
+    @Assisted override val id: InstanceGoalId,
+    @param:Assisted override val goal: SpecificationGoal
+) : InstanceGoal, DefaultEntity() {
+    internal data class DefaultGoalParameter(
+        private val map: MutableMap<Any, Any> = mutableMapOf()
+    ) : InstanceGoal.GoalParameter, Map<Any, Any> by map {
+        override fun add(key: Any, value: Any) {
+            map[key] = value
+        }
+
+        override fun remove(key: Any) {
+            map.remove(key)
+        }
+    }
+
+    override val parameters: InstanceGoal.GoalParameter = DefaultGoalParameter()
+}
+
+internal data class DefaultPmf @Inject constructor(
+    @Assisted override val id: PmfId
+) : Pmf, DefaultEntity()
+
+internal data class DefaultPolicy @Inject constructor(
+    @Assisted override val id: PolicyId
+) : Policy, DefaultEntity()
+
+internal data class DefaultRole @Inject constructor(
+    @Assisted override val id: RoleId
+) : Role, DefaultEntity()
+
+internal data class DefaultSpecificationGoal @Inject constructor(
+    @Assisted override val id: SpecificationGoalId
+) : SpecificationGoal, DefaultEntity()
--- chazm-model/src/main/kotlin/runtimemodels/chazm/model/koin/KoinModule.kt
+++ chazm-model/src/main/kotlin/runtimemodels/chazm/model/koin/KoinModule.kt
@@ -3,47 +3,74 @@ package runtimemodels.chazm.model.koin
 import org.koin.dsl.module.module
 import runtimemodels.chazm.api.entity.*
 import runtimemodels.chazm.api.id.*
+import runtimemodels.chazm.api.organization.Organization
 import runtimemodels.chazm.api.relation.*
 import runtimemodels.chazm.model.entity.*
+import runtimemodels.chazm.model.organization.DefaultOrganization
+import runtimemodels.chazm.model.parsers.XmlParser
 import runtimemodels.chazm.model.relation.*
 import javax.xml.stream.XMLInputFactory
 
+val EntityModule = module(path = DefaultEntity::class.java.packageName) {
+    factory<Agent> { (id: AgentId) -> DefaultAgent(id = id) }
+    factory<Attribute> { (id: AttributeId, type: Attribute.Type) -> DefaultAttribute(id = id, type = type) }
+    factory<Capability> { (id: CapabilityId) -> DefaultCapability(id = id) }
+    factory<Characteristic> { (id: CharacteristicId) -> DefaultCharacteristic(id = id) }
+    factory<InstanceGoal> { (id: InstanceGoalId, goal: SpecificationGoal) -> DefaultInstanceGoal(id = id, goal = goal) }
+    factory<Pmf> { (id: PmfId) -> DefaultPmf(id = id) }
+    factory<Policy> { (id: PolicyId) -> DefaultPolicy(id = id) }
+    factory<Role> { (id: RoleId) -> DefaultRole(id = id) }
+    factory<SpecificationGoal> { (id: SpecificationGoalId) -> DefaultSpecificationGoal(id = id) }
+}
+
+val ParserModule = module(path = XmlParser::class.java.packageName) {
+    single<XMLInputFactory> { XMLInputFactory.newInstance() }
+}
+
+val RelationModule = module(path = DefaultRelation::class.java.packageName) {
+    factory<Achieves> { (role: Role, goal: SpecificationGoal) -> AchievesRelation(role = role, goal = goal) }
+    factory<Assignment> { (agent: Agent, role: Role, goal: InstanceGoal) -> AssignmentRelation(agent = agent, role = role, goal = goal) }
+    factory<Contains> { (role: Role, characteristic: Characteristic, value: Double) -> ContainsRelation(role = role, characteristic = characteristic, value = value) }
+    factory<Has> { (agent: Agent, attribute: Attribute, value: Double) -> HasRelation(agent = agent, attribute = attribute, value = value) }
+    factory<Moderates> { (pmf: Pmf, attribute: Attribute) -> ModeratesRelation(pmf = pmf, attribute = attribute) }
+    factory<Needs> { (role: Role, attribute: Attribute) -> NeedsRelation(role = role, attribute = attribute) }
+    factory<Possesses> { (agent: Agent, capability: Capability, score: Double) -> PossessesRelation(agent = agent, capability = capability, score = score) }
+    factory<Requires> { (role: Role, capability: Capability) -> RequiresRelation(role = role, capability = capability) }
+    factory<Uses> { (role: Role, pmf: Pmf) -> UsesRelation(role = role, pmf = pmf) }
+
+}
+
 val KoinModule = module {
 
-    val EntityModule = module("entity") {
-        factory<Agent> { (id: AgentId) -> AgentEntity(id = id) }
-        factory<Attribute> { (id: AttributeId, type: Attribute.Type) -> AttributeEntity(id = id, type = type) }
-        factory<Capability> { (id: CapabilityId) -> CapabilityEntity(id = id) }
-        factory<Characteristic> { (id: CharacteristicId) -> CharacteristicEntity(id = id) }
-        factory<InstanceGoal> { (id: InstanceGoalId, goal: SpecificationGoal) -> InstanceGoalEntity(id = id, goal = goal) }
-        factory<Pmf> { (id: PmfId) -> PmfEntity(id = id) }
-        factory<Policy> { (id: PolicyId) -> PolicyEntity(id = id) }
-        factory<Role> { (id: RoleId) -> RoleEntity(id = id) }
-        factory<SpecificationGoal> { (id: SpecificationGoalId) -> SpecificationGoalEntity(id = id) }
-    }
 
     val OrganizationModule = module(path = "organization") {
-        //    factory<Organization> {
-//        DefaultOrganization(
-//            it
-//        )
-//    }
+        factory<Organization> {
+            DefaultOrganization(
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get(),
+                get()
+            )
+        }
+//        factory { create<DefaultOrganization>() }
     }
 
-    val ParserModule = module(path = "parser") {
-        single<XMLInputFactory> { XMLInputFactory.newInstance() }
-    }
-
-    val RelationModule = module(path = "relation") {
-        factory<Achieves> { (role: Role, goal: SpecificationGoal) -> AchievesRelation(role = role, goal = goal) }
-        factory<Assignment> { (agent: Agent, role: Role, goal: InstanceGoal) -> AssignmentRelation(agent = agent, role = role, goal = goal) }
-        factory<Contains> { (role: Role, characteristic: Characteristic, value: Double) -> ContainsRelation(role = role, characteristic = characteristic, value = value) }
-        factory<Has> { (agent: Agent, attribute: Attribute, value: Double) -> HasRelation(agent = agent, attribute = attribute, value = value) }
-        factory<Moderates> { (pmf: Pmf, attribute: Attribute) -> ModeratesRelation(pmf = pmf, attribute = attribute) }
-        factory<Needs> { (role: Role, attribute: Attribute) -> NeedsRelation(role = role, attribute = attribute) }
-        factory<Possesses> { (agent: Agent, capability: Capability, score: Double) -> PossessesRelation(agent = agent, capability = capability, score = score) }
-        factory<Requires> { (role: Role, capability: Capability) -> RequiresRelation(role = role, capability = capability) }
-        factory<Uses> { (role: Role, pmf: Pmf) -> UsesRelation(role = role, pmf = pmf) }
-
-    }
 }
