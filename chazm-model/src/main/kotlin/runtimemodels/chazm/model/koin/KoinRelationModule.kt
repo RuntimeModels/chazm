28e50912a62e8bcfd05ff0e7f92d99622ea29463
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-10 23:12:44 +0800

    #12: split up the RelationFactory into the individual relations
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/src/main/kotlin/runtimemodels/chazm/model/koin/KoinRelationModule.kt
+++ chazm-model/src/main/kotlin/runtimemodels/chazm/model/koin/KoinRelationModule.kt
@@ -7,41 +7,87 @@ import org.koin.standalone.KoinComponent
 import org.koin.standalone.get
 import runtimemodels.chazm.api.entity.*
 import runtimemodels.chazm.api.relation.*
-import runtimemodels.chazm.model.relation.RelationFactory
+import runtimemodels.chazm.model.relation.*
 import runtimemodels.chazm.model.relation.impl.*
 
 private class KoinRelationModule
 
-private class KoinRelationFactory : RelationFactory, KoinComponent {
+private class KoinAchievesFactory : AchievesFactory, KoinComponent {
     override fun build(role: Role, goal: SpecificationGoal): Achieves =
         get(parameters = { parametersOf(role, goal) })
+}
 
+private class KoinAssignmentFactory : AssignmentFactory, KoinComponent {
     override fun build(agent: Agent, role: Role, goal: InstanceGoal): Assignment =
         get(parameters = { parametersOf(agent, role, goal) })
+}
 
+private class KoinContainsFactory : ContainsFactory, KoinComponent {
     override fun build(role: Role, characteristic: Characteristic, value: Double): Contains =
         get(parameters = { parametersOf(role, characteristic, value) })
+}
 
+private class KoinHasFactory : HasFactory, KoinComponent {
     override fun build(agent: Agent, attribute: Attribute, value: Double): Has =
         get(parameters = { parametersOf(agent, attribute, value) })
+}
 
+private class KoinModeratesFactory : ModeratesFactory, KoinComponent {
     override fun build(pmf: Pmf, attribute: Attribute): Moderates =
         get(parameters = { parametersOf(pmf, attribute) })
+}
 
+private class KoinNeedsFactory : NeedsFactory, KoinComponent {
     override fun build(role: Role, attribute: Attribute): Needs =
         get(parameters = { parametersOf(role, attribute) })
+}
 
+private class KoinPossessesFactory : PossessesFactory, KoinComponent {
     override fun build(agent: Agent, capability: Capability, score: Double): Possesses =
         get(parameters = { parametersOf(agent, capability, score) })
+}
 
+private class KoinRequiresFactory : RequiresFactory, KoinComponent {
     override fun build(role: Role, capability: Capability): Requires =
         get(parameters = { parametersOf(role, capability) })
+}
 
+private class KoinUsesFactory : UsesFactory, KoinComponent {
     override fun build(role: Role, pmf: Pmf): Uses =
         get(parameters = { parametersOf(role, pmf) })
 }
 
+private class KoinRelationFactory(
+    achievesFactory: AchievesFactory,
+    assignmentFactory: AssignmentFactory,
+    containsFactory: ContainsFactory,
+    hasFactory: HasFactory,
+    moderatesFactory: ModeratesFactory,
+    needsFactory: NeedsFactory,
+    possessesFactory: PossessesFactory,
+    requiresFactory: RequiresFactory,
+    usesFactory: UsesFactory
+) : RelationFactory, KoinComponent,
+    AchievesFactory by achievesFactory,
+    AssignmentFactory by assignmentFactory,
+    ContainsFactory by containsFactory,
+    HasFactory by hasFactory,
+    ModeratesFactory by moderatesFactory,
+    NeedsFactory by needsFactory,
+    PossessesFactory by possessesFactory,
+    RequiresFactory by requiresFactory,
+    UsesFactory by usesFactory
+
 val RelationFactoryModule = module(KoinRelationModule::class.java.packageName) {
+    single<AchievesFactory> { create<KoinAchievesFactory>() }
+    single<AssignmentFactory> { create<KoinAssignmentFactory>() }
+    single<ContainsFactory> { create<KoinContainsFactory>() }
+    single<HasFactory> { create<KoinHasFactory>() }
+    single<ModeratesFactory> { create<KoinModeratesFactory>() }
+    single<NeedsFactory> { create<KoinNeedsFactory>() }
+    single<PossessesFactory> { create<KoinPossessesFactory>() }
+    single<RequiresFactory> { create<KoinRequiresFactory>() }
+    single<UsesFactory> { create<KoinUsesFactory>() }
     single<RelationFactory> { create<KoinRelationFactory>() }
 }
 
