b9572d9cc8898b6f0279348c8a63112fdde7932a
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-11 22:57:50 +0800

    #12: remove unused code
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- chazm-model/src/main/kotlin/runtimemodels/chazm/model/exceptions/EntityExistsException.kt
+++
@@ -1,8 +0,0 @@
-package runtimemodels.chazm.model.exceptions
-
-import runtimemodels.chazm.api.id.UniqueId
-import runtimemodels.chazm.model.message.E
-
-class EntityExistsException(
-    val id: UniqueId<*>
-) : IllegalArgumentException(E.ENTITY_ALREADY_EXISTS[id.type, id])
--- chazm-model/src/main/kotlin/runtimemodels/chazm/model/exceptions/EntityNotExistsException.kt
+++
@@ -1,8 +0,0 @@
-package runtimemodels.chazm.model.exceptions
-
-import runtimemodels.chazm.api.id.UniqueId
-import runtimemodels.chazm.model.message.E
-
-class EntityNotExistsException(
-    val id: UniqueId<*>
-) : IllegalArgumentException(E.ENTITY_DOES_NOT_EXISTS[id.type, id])
